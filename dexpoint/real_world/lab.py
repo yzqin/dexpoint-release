d3e3735ca7e0f1500b34ff6764d3ebea21b53757
wangyiji20 <3014193247@qq.com>
2024-04-11 18:02:13 +0800

    change carmera pose settings

--- dexpoint/real_world/lab.py
+++ dexpoint/real_world/lab.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sapien.core import Pose
 from scipy.spatial.transform import Rotation as R
-
+import transforms3d.quaternions
 CAM2ROBOT = Pose.from_transformation_matrix(np.array(
     [[0.60346958, 0.36270068, -0.7101216, 0.962396],
      [0.7960018, -0.22156729, 0.56328419, -0.35524235],
@@ -9,20 +9,23 @@ CAM2ROBOT = Pose.from_transformation_matrix(np.array(
      [0., 0., 0., 1.]]
 ))
 
-# CAM2ROBOT = Pose.from_transformation_matrix(np.array(
-
-#     [[-0.8660254, -0.       , -0.5       ,   0.60 -(-0.37+0.125)],
-#      [ 0.       ,  1.       , -0.        ,   0.0 - (-0.64 + 0.125)],
-#      [ 0.5      ,  0.       , -0.8660254 ,   0.35],
-#      [0., 0., 0., 1.]]
-# ))
+# CAM2ROBOT = Pose()
+# cam_rotation = R.from_quat([0.315856, -0.212542, 0.0472674, 0.923486])
+# CAM2ROBOT.set_p([0.962396, -0.355242, 0.318965])
+# CAM2ROBOT.set_q([0.315856, -0.212542, 0.0472674, 0.923486])
 
 
 CAM2WORLD = Pose()
-CAM2WORLD.set_p([0.35 + 0.37, 0.0, 0.35])
+CAM2WORLD.set_p([0.35 + 0.37, 0.0, 0.35])   # [0.35 + 0.37, 0.0, 0.35]
+CAM2WORLD.set_q([0.00412999, 0.186402, 0.000783578, -0.982465])
+# cam_rotation = R.from_euler('xyz', [-0.48, -21, 0.0], degrees=True)
+# CAM2WORLD.set_rotation(cam_rotation.as_matrix())
 
-cam_rotation = R.from_euler('xyz', [0, 0, np.pi/2],)
-CAM2WORLD.set_rotation(cam_rotation.as_matrix())
+
+# sapien2opencv = np.array([[0, -1, 0], [0, 0, -1], [1, 0, 0]])
+# sapien2opencv_quat = transforms3d.quaternions.mat2quat(sapien2opencv)
+# pose_cam = CAM2WORLD * Pose(q=sapien2opencv_quat)
+# print(pose_cam.to_transformation_matrix())
 
 DESK2ROBOT_Z_AXIS = 0.0   # -0.05
 
@@ -37,3 +40,6 @@ ROBOT2BASE = Pose(p=np.array([-0.37+0.125, -0.64 + 0.125, -DESK2ROBOT_Z_AXIS])) 
 TABLE_XY_SIZE = np.array([0.74, 1.28])    # origin 0.6, 1.2
 TABLE_ORIGIN = np.array([0.0, 0.0]) # origin 0, -0.15
 
+
+# camera = add_camera(name="", width=1024, height=768, fovy=1.57, near=0.1, far=100)
+# camera.set_local_pose(Pose([1.15312, -0.0071789, 0.5284], [0.00777893, -0.347578, 0.00288369, 0.937614]))
